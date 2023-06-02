741338a288ea4066eca25d1aca2e34ed27c540e3
Leonardo Isso <leoisso.work@gmail.com>
2023-06-02 00:35:14 -0300

    feat: add initial interaction with openai

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import { Database } from "sqlite3";
 
-import dataSource from "./typeorm"
+import dataSource from "./typeorm";
+import openai from "./openai";
+import personality from "./personality";
+
 
 const db = new Database("sqlite.db");
 dataSource.initialize();
 db.close();
+
+openai.createChatCompletion({
+  model: process.env.OPENAI_API_MODEL || "gpt-3.5-turbo",
+  messages: [
+    {
+      role: "system",
+      content: personality
+    },
+    {
+      role: "user",
+      content: "Hey, who are you? Can you tell me how was your day today?"
+    }
+  ],
+  temperature: 0,
+}).then(res => console.log(res.data.choices.map(c => console.log(c))));
