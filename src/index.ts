8adcbeb441923ecb4c8263ee27526379ef4930cf
Leonardo Isso <leoisso.work@gmail.com>
2023-06-02 22:57:25 -0300

    chore: clean index

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,7 @@ import * as dotenv from "dotenv";
 import { Database } from "sqlite3";
 
 import server from "./server";
-import openai from "./openai";
-import { getPersonalityMessage } from "./openai/personality";
 import { AppDataSource } from "./database/typeorm";
-import bot from "./bot";
 
 // Environment setup
 dotenv.config();
@@ -16,23 +13,11 @@ AppDataSource.initialize();
 db.close();
 
 // Server setup
-const host = process.env.SERVER_PORT;
+const host = process.env.SERVER_HOST;
 const port = process.env.SERVER_PORT;
 server.listen(port, () => {
-  console.log(`Server is running at ${host}${port}`);
+  console.log(`Server is running at ${host}:${port}`);
 });
 
-// Bot setup
-bot.start();
-
-openai.createChatCompletion({
-  model: process.env.OPENAI_API_MODEL || "gpt-3.5-turbo",
-  messages: [
-    getPersonalityMessage(),
-    {
-      role: "system",
-      content: "Introduce yourself, greet the user, and ask him how he is doing."
-    }
-  ],
-  temperature: 0,
-}).then(res => console.log(res.data.choices.map(c => console.log(c))));
+// Bot setup with long polling
+// bot.start();
