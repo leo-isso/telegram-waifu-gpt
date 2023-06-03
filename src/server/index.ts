e29d6f7e5205d19b5b38acd4777dd4638d197e62
Leonardo Isso <leoisso.work@gmail.com>
2023-06-02 21:52:37 -0300

    feat: add server middlewares

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,12 @@
 import * as express from "express";
 import { Express } from "express";
+import { webhookCallback } from "grammy";
+
+import telegramBot from "../bot";
 
 const server: Express = express();
+
+server.use(express.json());
+server.use(webhookCallback(telegramBot, "express"));
 
 export default server;
