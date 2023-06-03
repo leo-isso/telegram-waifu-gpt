9bee3a65a8f1378e3bd4b60f5ba9aa24d0e1d63b
Leonardo Isso <leoisso.work@gmail.com>
2023-06-02 21:43:50 -0300

    feat: implement server and bot setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 import * as dotenv from "dotenv";
 import { Database } from "sqlite3";
 
+import server from "./server";
 import openai from "./openai/openai";
 import { getPersonalityMessage } from "./openai/personality";
 import { AppDataSource } from "./database/typeorm";
+import bot from "./bot";
 
+// Environment setup
 dotenv.config();
 
+// Database setup
 const db = new Database("sqlite.db");
 AppDataSource.initialize();
 db.close();
+
+// Server setup
+const host = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT;
+server.listen(port, () => {
+  console.log(`Server is running at ${host}${port}`);
+});
+
+// Bot setup
+bot.start();
 
 openai.createChatCompletion({
   model: process.env.OPENAI_API_MODEL || "gpt-3.5-turbo",
