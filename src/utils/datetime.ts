697afea8fa24f02ce8203987e45e450ae1d58332
Leonardo Isso <leoisso.work@gmail.com>
2023-06-08 20:47:24 -0300

    fix: remove math

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -2,7 +2,8 @@ export function dateNowUnixTime() {
   const dateNow = new Date();
   return Math.floor(dateNow.getTime() / 1000);
 }
+
 export function dateNowToTimestamp() {
   const dateNow = new Date();
-  return Math.floor(dateNow.getTime() / 1000);
+  return Math.floor(dateNow.getTime());
 }
