673c2dd8bc8412c56ceef688e459a1537502aa5c
Leonardo Isso <leoisso.work@gmail.com>
2023-06-08 01:09:50 -0300

    feat: add and test Message history creation

--- src/bot/events.ts
+++ src/bot/events.ts
@@ -5,14 +5,15 @@ import ChatService from "../domains/chat/service";
 import openai from "../openai";
 import nyanna from "../openai/personalities/nyanna";
 import PersonalityPrompt from "../openai/personalityPrompt";
+import MessageService from "../domains/message/service";
 
 const personalityPrompt = new PersonalityPrompt(nyanna).getPersonalityPrompt();
 const chatService = new ChatService();
+const messageService = new MessageService();
 
 export function initTelegramBot(telegramBot: TelegramBot) {
   telegramBot.bot.command("start",
     async (ctx) => {
-      // Gets ChatGPT response
       const messages: ChatCompletionRequestMessage[] = [
         ...personalityPrompt,
         {
@@ -20,17 +21,28 @@ export function initTelegramBot(telegramBot: TelegramBot) {
           content: "Introduce yourself, greet the user, and ask him how he is doing."
         }
       ];
+
+      // Creates or get user
+      const userId = ctx.from?.id;
+      if (userId) {
+        const chat = await chatService.getOrCreate(userId);
+        // Creates Messages history
+        if (chat) {
+          const promises = messages.map(message => messageService.create(
+            message.role,
+            message.content,
+            chat.id
+          ));
+          Promise.all(promises);
+        }
+      }
+
+      // Gets ChatGPT response
       const gpt_response = await openai.createChatCompletion(messages);
       const chat_gpt_answer = gpt_response.data.choices[0]?.message?.content || "Not available.";
 
       // Replies user
       ctx.reply(chat_gpt_answer);
-
-      // Creates or get user
-      const userId = ctx.from?.id;
-      if (userId) {
-        chatService.getOrCreate(userId);
-      }
     });
 
   telegramBot.bot.on("message",
