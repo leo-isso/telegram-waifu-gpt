c6c0ea598399c921525831143f32da215fd71ed8
Leonardo Isso <leoisso.work@gmail.com>
2023-06-08 20:46:10 -0300

    feat: add dateNowToTimestamp

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -2,3 +2,7 @@ export function dateNowUnixTime() {
   const dateNow = new Date();
   return Math.floor(dateNow.getTime() / 1000);
 }
+export function dateNowToTimestamp() {
+  const dateNow = new Date();
+  return Math.floor(dateNow.getTime() / 1000);
+}
