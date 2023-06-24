52e051edcf51e3435b4b93fe026513957165bc2e
Leonardo Isso <leoisso.work@gmail.com>
2023-06-24 17:49:20 -0300

    feat: refactor client implemention

--- src/domains/message/cache.ts
+++ src/domains/message/cache.ts
@@ -1,25 +1,22 @@
-import redis, { RedisClient } from "../../cache";
+import { cache } from "../..";
+import { RedisClient } from "../../cache";
 import { ChatCompletionMessage } from "./messages.types";
 
 class MessageCache {
-  private client: RedisClient;
-
-  constructor() {
-    this.client = redis;
-  }
+  private redisClient: RedisClient = cache.redis;
 
   async create(chatId: string, data: ChatCompletionMessage[]): Promise<void> {
     const listKey = this._buildListKey(chatId);
     const listValues = this._buildListValues(data);
     
     listValues.forEach(message => {
-      this.client.rPush(listKey, message);
+      this.redisClient.rPush(listKey, message);
     });
   }
 
   async findOne(chatId: string, count=-10): Promise<ChatCompletionMessage[]> {
     const listKey = this._buildListKey(chatId);
-    const messages = await this.client.lRange(listKey, count, -1);
+    const messages = await this.redisClient.lRange(listKey, count, -1);
     return messages.map(message => JSON.parse(message));
   }
 
