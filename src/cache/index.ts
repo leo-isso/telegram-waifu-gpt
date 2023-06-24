7916b9a8566de38dad6d100cb4ce6f10a00ddb6e
Leonardo Isso <leoisso.work@gmail.com>
2023-06-24 17:22:15 -0300

    fix: improve redis config in order to work with containers

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -3,7 +3,17 @@ import { createClient } from "redis";
 export type RedisClient = ReturnType<typeof createClient>
 
 const redis = createClient({
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT || "6379")
+  },
   password: process.env.REDIS_PASSWORD
 });
+
+redis.on("connect", () => {
+  console.log("Connected to Redis server");
+});
+
+redis.connect();
 
 export default redis;
