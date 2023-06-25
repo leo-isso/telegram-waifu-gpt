a1278c0afe3e9972966df5e3199850561e11fbb2
Leonardo Isso <leoisso.work@gmail.com>
2023-06-25 17:49:41 -0300

    fix: update private methods and change message send strategy

--- src/bot/actions/start.ts
+++ src/bot/actions/start.ts
@@ -1,31 +1,25 @@
-import { ChatCompletionResponseMessage } from "openai";
 import { Chat } from "../../domains/chat/entities";
 import ChatService from "../../domains/chat/service";
 import { ChatCompletionMessage } from "../../domains/message/messages.types";
 import MessageService from "../../domains/message/service";
 import PromptComposer from "../../openai/prompts/PromptComposer";
-import { BotActionsInterface } from "./BotActionsInterface";
+import { IBotActions } from "./BotActionsInterface";
 import openai from "../../openai";
-import { Message } from "grammy/types";
 
 const START_PROMPT: ChatCompletionMessage = {
   role: "system",
   content: "Introduce yourself, greet the user, and ask him how he is doing."
 };
 
-class StartAction implements BotActionsInterface {
+class StartAction implements IBotActions {
   userId: number;
-  messageSender: (text: string) => Promise<Message.TextMessage>;
+  message: string | null = null;
 
-  constructor(
-    userId: number,
-    messageSender: (text: string) => Promise<Message.TextMessage>
-  ) {
+  constructor(userId: number) {
     this.userId = userId;
-    this.messageSender = messageSender;
   }
 
-  async saveMessages(chatId: string, prompts: ChatCompletionMessage[]): Promise<void> {
+  private async saveMessages(chatId: string, prompts: ChatCompletionMessage[]): Promise<void> {
     for (const prompt of prompts) {
       await MessageService.create(
         prompt.role,
@@ -35,31 +29,31 @@ class StartAction implements BotActionsInterface {
     } 
   }
 
-  async getOrCreateChat(): Promise<Chat> {
+  private async getOrCreateChat(): Promise<Chat> {
     return await ChatService.getOrCreate(this.userId);
   }
 
-  composeMessages(newPrompts: ChatCompletionMessage[]): ChatCompletionMessage[] {
+  private composeMessages(newPrompts: ChatCompletionMessage[]): ChatCompletionMessage[] {
     const promptComposer = new PromptComposer(newPrompts);
     return promptComposer.compose();
   }
 
-  async getAIResponse(prompts: ChatCompletionMessage[]): Promise<ChatCompletionMessage | null> {
+  private async getAIResponse(prompts: ChatCompletionMessage[]): Promise<ChatCompletionMessage | null> {
     const gpt_response = await openai.createChatCompletion(prompts);
     return gpt_response.data.choices[0]?.message || null;
   }
 
-  async sendMessage(): Promise<void> {
+  async processMessage(): Promise<void> {
     const chat = await this.getOrCreateChat();
     const prompts = this.composeMessages([START_PROMPT]);
     const AIResponse = await this.getAIResponse(prompts);
 
     if (AIResponse) {
-      await this.messageSender(AIResponse.content);
+      this.message = AIResponse.content;
       const newMessages = [...prompts, AIResponse];
       await this.saveMessages(chat.id, newMessages);
     } else {
-      await this.messageSender("Sorry, an error occurred.");
+      this.message = "Sorry, an error occurred.";
     }
   }
 }
