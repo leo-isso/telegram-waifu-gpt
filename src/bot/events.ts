72fa9bf6046d3a9a158ec12ce7944461c2a6fec8
Leonardo Isso <leoisso.work@gmail.com>
2023-06-25 17:50:24 -0300

    feat: refactor start action using new StartAction

--- src/bot/events.ts
+++ src/bot/events.ts
@@ -7,39 +7,19 @@ import MessageService from "../domains/message/service";
 import MessageCache from "../domains/message/cache";
 import { ChatCompletionMessage } from "../domains/message/messages.types";
 import PromptComposer from "../openai/prompts/PromptComposer";
+import StartAction from "./actions/start";
 
 export function initTelegramBot(telegramBot: TelegramBot) {
-  telegramBot.bot.command("start",
-    async (ctx) => {
-      const promptComposer = new PromptComposer([
-        {
-          role: "system",
-          content: "Introduce yourself, greet the user, and ask him how he is doing."
-        }
-      ]);
-      const messages = promptComposer.compose();
-
-      // Creates or get user
-      const userId = ctx.from?.id;
-      if (userId) {
-        const chat = await ChatService.getOrCreate(userId);
-        // Creates Messages history
-        for (const message of messages) {
-          await MessageService.create(
-            message.role,
-            message.content,
-            chat.id
-          );
-        }
-      }
-
-      // Gets ChatGPT response
-      const gpt_response = await openai.createChatCompletion(messages);
-      const chat_gpt_answer = gpt_response.data.choices[0]?.message?.content || "Not available.";
-
-      // Replies user
-      ctx.reply(chat_gpt_answer);
-    });
+  telegramBot.bot.command("start", async ctx => {
+    const userId = ctx.from?.id;
+    if(userId) {
+      const startAction = new StartAction(userId);
+      await startAction.processMessage();
+      ctx.reply(startAction.message || "Something went wrong...");
+    } else {
+      ctx.reply("Sorry, could not find your userId");
+    }
+  });
 
   telegramBot.bot.on("message",
     async (ctx) => {
