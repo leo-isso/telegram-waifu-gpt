448282ee832b002c404d9ecb4f62eeb03c21743a
Leonardo Isso <leoisso.work@gmail.com>
2023-06-25 17:48:35 -0300

    fix: remove private methods from Interface

--- src/bot/actions/BotActionsInterface.ts
+++ src/bot/actions/BotActionsInterface.ts
@@ -1,13 +1,5 @@
-import { Chat } from "../../domains/chat/entities";
-import { ChatCompletionMessage } from "../../domains/message/messages.types";
-import { Message } from "grammy/types";
-
-export interface BotActionsInterface {
+export interface IBotActions {
   userId: number,
-  composeMessages(newPrompts: ChatCompletionMessage[]): ChatCompletionMessage[], 
-  getOrCreateChat?(): Promise<Chat>,
-  getAIResponse(prompts: ChatCompletionMessage[]): Promise<ChatCompletionMessage | null>,
-  messageSender: (text: string) => Promise<Message.TextMessage>,
-  saveMessages(chatId: string, prompts: ChatCompletionMessage[]): Promise<void>,
-  sendMessage(): Promise<void>,
+  message: string | null,
+  processMessage(): Promise<void>,
 }
