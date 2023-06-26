06171aea939333a3972e382fbdeab60418950f41
Leonardo Isso <leoisso.work@gmail.com>
2023-06-25 21:01:19 -0300

    feat: add saving and handling new messages methods

--- src/bot/actions/BaseAction.ts
+++ src/bot/actions/BaseAction.ts
@@ -1,4 +1,5 @@
 import { Chat } from "../../domains/chat/entities";
+import MessageCache from "../../domains/message/cache";
 import ChatService from "../../domains/chat/service";
 import { ChatCompletionMessage } from "../../domains/message/messages.types";
 import MessageService from "../../domains/message/service";
@@ -7,9 +8,31 @@ import openai from "../../openai";
 
 class BaseAction {
   userId: number;
+  newMessage: string | null;
+  messageCache = new MessageCache();
 
-  constructor(userId: number) {
+  constructor(userId: number, newMessage?: string) {
     this.userId = userId;
+    this.newMessage = newMessage || null;
+  }
+
+  async cacheMessages(chatId:string, prompts: ChatCompletionMessage[]): Promise<void> {
+    await this.messageCache.create(chatId, prompts);
+  }
+
+  async getLatestMessages(chatId: string): Promise<ChatCompletionMessage[]>{
+    const cachedLatestMessages: ChatCompletionMessage[] = await this.messageCache.findLatest(chatId);
+    
+    if (cachedLatestMessages.length > 0) {
+      return cachedLatestMessages;
+    }
+    
+    return (await MessageService.getLatestMessages(chatId))
+      .map(message => ({
+        role: message.role,
+        content: message.message
+      }))
+      .reverse();
   }
 
   async saveMessages(chatId: string, prompts: ChatCompletionMessage[]): Promise<void> {
@@ -26,8 +49,22 @@ class BaseAction {
     return await ChatService.getOrCreate(this.userId);
   }
 
-  composeMessages(newPrompts: ChatCompletionMessage[]): ChatCompletionMessage[] {
-    const promptComposer = new PromptComposer(newPrompts);
+  getNewMessageAsCompletionMessage(): ChatCompletionMessage[] {
+    if (!this.newMessage) {
+      return [];
+    }
+    return [{
+      role: "user",
+      content: this.newMessage,
+    }];
+  }
+
+  composeMessages(
+    newPrompts: ChatCompletionMessage[],
+    latestPrompts: ChatCompletionMessage[]=[],
+    includePersonality=true,
+  ): ChatCompletionMessage[] {
+    const promptComposer = new PromptComposer(newPrompts, latestPrompts, includePersonality);
     return promptComposer.compose();
   }
 
