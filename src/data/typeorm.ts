82f83bc57add016a715dcdb24d880b6e7bf08278
Leonardo Isso <leoisso.work@gmail.com>
2023-08-06 20:01:21 -0300

    feat: update DataSource instance

--- src/data/typeorm.ts
+++ src/data/typeorm.ts
@@ -3,11 +3,12 @@ import { Chat } from "../domains/chat/entities";
 import { Message } from "../domains/message/entities";
 
 export const AppDataSource = new DataSource({
-  type: "sqlite",
-  database: "sqlite.db",
+  type: "postgres",
+  port: parseInt(process.env.POSTGRES_PORT || ""),
+  database: process.env.POSTGRES_DATABASE,
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
   synchronize: true,
   logging: false,
-  entities: [Chat, Message],
-  subscribers: [],
-  migrations: [],
+  entities: [Chat, Message]
 });
